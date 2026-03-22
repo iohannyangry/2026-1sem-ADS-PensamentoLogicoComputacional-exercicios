5ec2f674f4308c089b9e9735e5111a664994bc5d
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-22 18:45:48 -0300

    Update exercicio2.py

--- 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-2/exercicio2.py
+++ 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-2/exercicio2.py
@@ -1,1 +1,16 @@
 #exercicio2
+>>> # O range começa em 10, vai até 101 (para incluir o 100) e pula de 10 em 10
+>>>
+>>> for numero in range(10, 101, 10):
+...             print(numero)
+...
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
