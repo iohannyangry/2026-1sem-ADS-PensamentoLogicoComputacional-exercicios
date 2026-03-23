54c737fc5778bb763c6567e5dad65f0186dd6ba3
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-22 23:04:14 -0300

    Update exercicio7.py

--- 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-7/exercicio7.py
+++ 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-7/exercicio7.py
@@ -1,1 +1,11 @@
 #exercicio7
+>>> temperatura = float(input("Digite a temperatura em graus Celsius: "))
+Digite a temperatura em graus Celsius: 35
+>>> if temperatura < 15:
+...                 print("Frio")
+... elif temperatura <= 25:
+...                 print("Agradável")
+... else:
+...                 print("Quente")
+...
+Quente
