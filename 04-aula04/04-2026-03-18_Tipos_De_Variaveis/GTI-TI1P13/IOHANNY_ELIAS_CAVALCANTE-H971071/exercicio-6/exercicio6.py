8e36ca69ddfb08983f9b4ef0333dcc80778498c3
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 11:42:48 -0300

    Update exercicio6.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-6/exercicio6.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-6/exercicio6.py
@@ -1,1 +1,17 @@
 #exercicio6
+ numeros = []
+>>> for i in range(5):
+...     num = int(input(f"digite o {i+1}º número: "))
+...     numeros.append(num)
+...
+digite o 1º número: 7
+digite o 2º número: 8
+digite o 3º número: 20
+digite o 4º número: 50
+digite o 5º número: 45
+>>> if 25 in numeros:
+...     print("O número 25 está presente na lista.")
+... else:
+...      print("O número 25 NÃO está presente na lista.")
+...
+O número 25 NÃO está presente na lista.
