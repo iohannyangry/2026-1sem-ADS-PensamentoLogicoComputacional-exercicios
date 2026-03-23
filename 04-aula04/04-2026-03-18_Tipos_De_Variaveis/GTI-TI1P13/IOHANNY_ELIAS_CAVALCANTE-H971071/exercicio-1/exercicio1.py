aa9f6f7312135fcd674afa4775626be6b916182a
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 10:19:18 -0300

    Update exercicio1.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-1/exercicio1.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-1/exercicio1.py
@@ -1,1 +1,14 @@
 #exercicio1
+ num1 = float(input("digite o primeiro número: "))
+digite o primeiro número: 50
+>>> num2 = float(input("digite o segundo número:  "))
+digite o segundo número:  10
+>>> soma = num1 + num2
+>>> subtracao = num1 - num2
+>>> resto = num1 % num2
+>>> print(f"soma: {soma}")
+soma: 60.0
+>>> print(f"subtração: {subtracao}")
+subtração: 40.0
+>>> print(f"resto da divisão: {resto}")
+resto da divisão: 0.0
