c1aa56587afd02b0ce054cddf9c5e08994a72c96
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-22 23:49:23 -0300

    Update exercicio8.py

--- 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-8/exercicio8.py
+++ 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-8/exercicio8.py
@@ -1,1 +1,18 @@
 #exercicio8
+ preco_unitario = 100.00
+>>>
+>>> quantidade = int(input("Quantas unidades você deseja comprar? "))
+Quantas unidades você deseja comprar? 57
+>>> valor_total = preco_unitario * quantidade
+>>> if quantidade >= 10:
+...       desconto = valor_total * 0.10
+...       valor_final = valor_total - desconto
+...       print(f"Você ganhou 10% de desconto! Valor economizado: R$ {desconto:.2f}")
+... else:
+...       valor_final = valor_total
+...       print("Quantidade insuficiente para desconto.")
+...
+...
+Você ganhou 10% de desconto! Valor economizado: R$ 570.00
+>>> print(f"O valor final da sua compra é: R$ {valor_final:.2f}")
+O valor final da sua compra é: R$ 5130.00
