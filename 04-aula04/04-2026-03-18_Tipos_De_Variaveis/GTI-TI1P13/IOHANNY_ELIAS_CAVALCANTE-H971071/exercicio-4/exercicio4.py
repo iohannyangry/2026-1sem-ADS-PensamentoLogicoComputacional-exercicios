16409ca7fb4ba5bd1ec2af2588e624a6854bc9ec
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 11:04:19 -0300

    Update exercicio4.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-4/exercicio4.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-4/exercicio4.py
@@ -1,1 +1,18 @@
 #exercicio4
+ idade = int(input("digite a sua idade: "))
+digite a sua idade: 24
+>>> if idade >= 18:
+...     print("A pessoa é maior de idade.")
+... else:
+...     print("A pessoa é menor de idade.")
+...
+A pessoa é maior de idade.
+
+ idade = int(input("digite a sua idade: "))
+digite a sua idade: 25
+>>> if idade >=60:
+...     print("A pessoa é idosa.")
+... else:
+...     print("A pessoa NÃO é idosa.")
+...
+A pessoa NÃO é idosa.
