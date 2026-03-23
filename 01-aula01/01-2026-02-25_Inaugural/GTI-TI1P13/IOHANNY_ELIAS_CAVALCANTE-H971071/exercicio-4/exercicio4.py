0f229f253bac8122ba9ae54da567f5ea8c4ed510
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-22 22:43:34 -0300

    Update exercicio4.py

--- 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-4/exercicio4.py
+++ 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-4/exercicio4.py
@@ -1,1 +1,9 @@
 #exercicio4
+>>> idade = int(input("Digite a sua idade: "))
+Digite a sua idade: 25
+>>> if idade >= 18:
+...         print("Acesso Liberado:")
+... else:
+...         print("Acesso Negado !")
+...
+Acesso Liberado:
