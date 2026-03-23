1543caeccb35ba3ef7c92922b449ab1d183ce38e
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 11:18:25 -0300

    Update exercicio5.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-5/exercicio5.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-5/exercicio5.py
@@ -1,1 +1,9 @@
 #exercicio5
+ entrada_string = input("digite um número decimal: ")
+digite um número decimal: 7
+>>> numero_float = float(entrada_string)
+>>> numero_int = int(numero_float)
+>>> print(f"valor em float: {numero_float}")
+valor em float: 7.0
+>>> print(f"valor em int: {numero_int}")
+valor em int: 7
