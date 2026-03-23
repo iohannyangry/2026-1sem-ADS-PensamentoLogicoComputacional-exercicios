1a01dd6bbabe723084a07d66559e9bd1a1849a0f
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-22 22:53:30 -0300

    Update exercicio6.py

--- 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-6/exercicio6.py
+++ 01-aula01/01-2026-02-25_Inaugural/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-6/exercicio6.py
@@ -1,1 +1,9 @@
 #exercicio6
+>>> numero = int(input("Digite um número inteiro: "))
+Digite um número inteiro: 7
+>>> if numero % 2 == 0:
+...         print(f"O número {numero} é Par.")
+... else:
+...         print(f"O número {numero} é Ímpar.")
+...
+O número 7 é Ímpar.
