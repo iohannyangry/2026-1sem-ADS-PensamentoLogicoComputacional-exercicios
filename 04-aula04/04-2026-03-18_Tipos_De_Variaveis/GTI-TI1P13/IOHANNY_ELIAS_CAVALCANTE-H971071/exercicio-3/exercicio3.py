2cbff96c6d2c4aea021b98740fbfdbdd9aa63d52
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 10:44:37 -0300

    Update exercicio3.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-3/exercicio3.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-3/exercicio3.py
@@ -1,1 +1,9 @@
 #exercicio3
+ numero = float(input("digite um número: "))
+digite um número: 70
+>>> if 10 <= numero <= 50:
+...     print("O número esta entre 10 e 50.")
+... else:
+...      print("O número NÃO esta entre 10 e 50.")
+...
+O número NÃO esta entre 10 e 50.
