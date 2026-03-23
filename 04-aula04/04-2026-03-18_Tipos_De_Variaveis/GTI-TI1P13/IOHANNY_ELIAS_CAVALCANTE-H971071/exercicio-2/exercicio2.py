68986103537e71508c61eac8c701d3ee539b9a69
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 11:30:49 -0300

    Update exercicio2.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-2/exercicio2.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-2/exercicio2.py
@@ -1,1 +1,22 @@
 #exercicio2
+ num1 = float(input("digite o primeiro número: "))
+digite o primeiro número: 95
+>>> num2 =  float(input("digite o segundo número: "))
+digite o segundo número: 50
+>>> if num1 == num2:
+...     print("Os números são iguais.")
+... else:
+...     print("Os números são diferentes.")
+...
+Os números são diferentes.
+
+   num1 = float(input("digite o primeiro número: "))
+digite o primeiro número: 50
+>>> num2 = float(input("digite o segundo número: "))
+digite o segundo número: 170
+>>> if num1 > num2:
+...     print(f"O maior número é: {num1}")
+... else:
+...     print(f"O maior número é: {num2}")
+...
+O maior número é: 170.0
