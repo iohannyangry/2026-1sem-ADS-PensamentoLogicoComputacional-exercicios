cb0c845a2984989b3a8ba16b3ae1184c81e14dd7
iohannyangry <iohanny.e.cavalcante@gmail.com>
2026-03-23 11:57:52 -0300

    Update exercicio7.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-7/exercicio7.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/IOHANNY_ELIAS_CAVALCANTE-H971071/exercicio-7/exercicio7.py
@@ -1,1 +1,10 @@
 #exercicio7
+ lista_a = [1,2,3]
+>>> lista_b = [1,2,3]
+>>> print(f"lista_a == lista_b: {lista_a == lista_b}")
+lista_a == lista_b: True
+>>> print(f"lista_a is lista_b: {lista_a is lista_b}")
+lista_a is lista_b: False
+
+lista_a == lista_b - true: Porque os valores dentro das listas são iguais.
+lista_a is lista_b - False: Porque são objetos diferentes na memória.
